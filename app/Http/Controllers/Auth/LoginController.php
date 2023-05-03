9dcb1772bb258f36bb026e4e0ee2993d2bf54b05
mayurjani91 <maddy.jani91@gmail.com>
2023-05-03 10:11:43 +0530

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+    
+    /*clear cashe if you want to clear this attempt before given time*/
+
+    //Define attempts and time 
+    protected $maxAttempts = 3; // Default is 5
+    protected $decayMinutes = 1440; // Default is 1 but we are adding 1440 for 24 hrs
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
